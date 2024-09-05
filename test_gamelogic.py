2c258c1c68aa44655d13c9a912735fbf50c02080
Juaniitrox2 <jdragan@alumno.huergo.edu.ar>
2024-09-05 15:55:38 -0300

    el toilet mas terminado

--- test_gamelogic.py
+++ test_gamelogic.py
@@ -2,14 +2,14 @@
 import unittest
 
 from Classes.exceptions import InvalidTileCoordinate, GameFull, GameEmpty, GameStarted
-from Classes.tatetilogic import GameLogic
+from Classes.tatetilogic import TatetiLogic
 from Classes.player import Player
-from Classes.tatetiboard import Board
+from Classes.tatetiboard import TatetiBoard
 
 
 class TestGameLogic(unittest.TestCase):
     def test_juego_tira_error_con_posicion_de_casilla_invalida(self):
-        newgamelogic = GameLogic(Board())
+        newgamelogic = TatetiLogic(TatetiBoard())
         plr1 = Player("X")
 
         newgamelogic.add_player(plr1)
@@ -18,7 +18,7 @@ class TestGameLogic(unittest.TestCase):
             newgamelogic.set_player_on_tile(plr1, (5, 12))
     
     def test_juego_tira_error_con_mas_de_dos_jugadores(self):
-        newgamelogic = GameLogic(Board())
+        newgamelogic = TatetiLogic(TatetiBoard())
         plr1 = Player("X")
         plr2 = Player("O")
         plr3 = Player("Y")
@@ -30,13 +30,13 @@ class TestGameLogic(unittest.TestCase):
             newgamelogic.add_player(plr3)
 
     def test_intentar_jugar_sin_suficientes_jugadores(self):
-        newgamelogic = GameLogic(Board())
+        newgamelogic = TatetiLogic(TatetiBoard())
 
         with self.assertRaises(GameEmpty):
             newgamelogic.play_turn((1, 1))
 
     def test_intentar_sacar_jugador_cuando_juego_comenzó(self):
-        newgamelogic = GameLogic(Board())
+        newgamelogic = TatetiLogic(TatetiBoard())
         plr1 = Player("X")
         plr2 = Player("O")
 
